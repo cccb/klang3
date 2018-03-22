b2a7569dcb60c6475eaf3b1bb4404b5c3ac64093
Matthias Hannig <matthias@hannig.cc>
2018-03-22 19:44:02 +0100

    moved actions; added service

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"os"
-	"time"
 )
 
 type Config struct {
--- service.go
+++ service.go
@@ -4,112 +4,44 @@ import (
 	"github.com/cameliot/alpaca"
 )
 
-/* Actions */
-const GROUPS_LIST_REQUEST = "@sampler/GROUPS_LIST_REQUEST"
-const GROUPS_LIST_SUCCESS = "@sampler/GROUPS_LIST_SUCCESS"
-const GROUPS_LIST_ERROR = "@sampler/GROUPS_LIST_ERROR"
-
-const SAMPLES_LIST_REQUEST = "@sampler/SAMPLES_LIST_REQUEST"
-const SAMPLES_LIST_SUCCESS = "@sampler/SAMPLES_LIST_SUCCESS"
-const SAMPLES_LIST_ERROR = "@sampler/SAMPLES_LIST_ERROR"
-
-const SAMPLE_START_REQUEST = "@sampler/SAMPLE_START_REQUEST"
-const SAMPLE_START_SUCCESS = "@sampler/SAMPLE_START_SUCCESS"
-const SAMPLE_START_ERROR = "@sampler/SAMPLE_START_ERROR"
-
-const SAMPLE_STOP_REQUEST = "@sampler/SAMPLE_STOP_REQUEST"
-const SAMPLE_STOP_SUCCESS = "@sampler/SAMPLE_STOP_SUCCESS"
-const SAMPLE_STOP_ERROR = "@sampler/SAMPLE_STOP_ERROR"
-
-/* Payloads */
-type GroupsPayload struct {
-	Groups []string `json:"groups"`
+type SamplerSvc struct {
+	repo *Repository
 }
 
-type SamplesPayload struct {
-	Group   string    `json:"group"`
-	Samples []*Sample `json:"samples"`
-}
-
-type SampleIdPayload struct {
-	SampleId int `json:"sample_id"`
-}
-
-type SamplePayload struct {
-	Sample *Sample `json:"sample"`
-}
-
-type ErrorPayload struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-type SampleIdErrorPayload struct {
-	SampleId int    `json:"sample_id"`
-	Code     int    `json:"code"`
-	Message  string `json:"message"`
-}
-
-/* Action Creators */
-func GroupsListSuccess(groups []string) aplaca.Action {
-	return alpaca.Action{
-		Type: GROUPS_LIST_SUCCESS,
-		Payload: GroupsPayload{
-			Groups: groups,
-		},
+func NewSamplerSvc(repository *Repository) *SamplerSvc {
+	svc := &SamplerSvc{
+		repo: repository,
 	}
+
+	return svc
 }
 
-func GroupsListError(code int, err error) alpaca.Action {
-	return alpaca.Action{
-		Type: GROUPS_LIST_ERROR,
-		Payload: ErrorPayload{
-			Code:    code,
-			Message: err.Error(),
-		},
+func (self *SamplerSvc) handleGroupsListRequest() alpaca.Action {
+	groups := self.repo.Groups()
+
+	return GroupsListSuccess(groups)
+}
+
+func (self *SamplerSvc) handleSamplesListRequest(request *SamplesListPayload) alpaca.Action {
+	samples := self.repo.Samples(request.Group)
+
+	return SamplesListSuccess(request.Group, samples)
+}
+
+func (self *SamplerSvc) Handle(actions alpaca.Actions, dispatch alpaca.Dispatch) {
+
+	for action := range actions {
+		switch action.Type {
+		case GROUPS_LIST_REQUEST:
+			dispatch(self.handleGroupsListRequest())
+			break
+
+		case SAMPLES_LIST_REQUEST:
+			var request SamplesListPayload
+			action.DecodePayload(&request)
+			dispatch(self.handleSamplesListRequest(&request))
+			break
+		}
 	}
-}
-
-func SamplesListSuccess(group string, samples []*Sample) alpaca.Action {
-	return alpaca.Action{
-		Type: SAMPLES_LIST_SUCCESS,
-		Payload: SamplesPayload{
-			Group:   group,
-			Samples: samples,
-		},
-	}
-}
-
-func SamplesListError(code int, err error) alpaca.Action {
-	return alpaca.Action{
-		Type: SAMPLES_LIST_ERROR,
-		Payload: ErrorPayload{
-			Code:    code,
-			Message: err.Error(),
-		},
-	}
-}
-
-func SampleStartSuccess(sampleId int) alpaca.Action {
-	return alpaca.Action{
-		Type: SAMPLE_START_SUCCESS,
-		Payload: SampleIdPayload{
-			SampleId: sampleId,
-		},
-	}
-}
-
-func SampleStartError(sampleId int, code int, err error) alpaca.Action {
-	return alpaca.Action{
-		Type: SAMPLE_START_SUCCESS,
-		Payload: SampleIdErrorPayload{
-			SampleId: sampleId,
-			Code:     code,
-			Message:  err.Error(),
-		},
-	}
-}
-
-func handle(actions alpaca.Actions, dispatch alpaca.Dispatch) {
 
 }
